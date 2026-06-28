10ec0dd3286037ef5ef6fd45d7bda34f72f6fe2a
Fady Mohsen <82896079+fadymohsen@users.noreply.github.com>
2026-06-28 23:15:20 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,9 @@
     include 'front.php';
     include 'dashboard.php';
 
-?>
+Route::get('/sitemap.xml', function () {
+    \App\Services\SiteMapService::generate();
+    return response()->file(public_path('sitemap.xml'), [
+        'Content-Type' => 'application/xml',
+    ]);
+});
